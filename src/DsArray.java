314347acea7694f5e53c8b83e276803da9afa91e
Gustavo Morais <gmoraisildefonso@gmail.com>
2025-03-10 21:17:56 -0300

    Desafio basicão de array

---
+++ src/DsArray.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class DsArray {
+    public static void main(String[] args) {
+        ArrayList<String> listaNome = new ArrayList<>(); // Definindo que a lista armazenará Strings
+
+        listaNome.add("Carlos");
+        listaNome.add("Ana");
+        listaNome.add("Bruno");
+
+        System.out.println("Lista de nomes: " + listaNome);
+    }
+}
